8305bfcf256899723cc2cb85dcf35cbb73e0e387
Josselyn <vanesitapolanco@gmail.com>
2022-11-07 12:40:55 -0600

    [ADD] Add function delete and hover

--- notes/src/app/notebook/list/list.component.ts
+++ notes/src/app/notebook/list/list.component.ts
@@ -1,32 +1,72 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Note } from 'src/app/interfaces/note.interface';
+import swal from 'sweetalert2';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css']
+  styleUrls: ['./list.component.css'],
 })
 export class ListComponent implements OnInit {
-
   note: Note | undefined;
-  listNotes : Array<Note> = new Array<Note>();
+  listNotes: Array<Note> = new Array<Note>();
+  showButton = false;
 
-  @Input() set notes(value: Note){
+  /*Cambiando valor de variable cuando es modificada por el componente padre*/
+
+  @Input() set notes(value: Note) {
     this.note = value;
-    if(this.note.note != ''){
-      this.listNotes.push(this.note)
+    if (this.note.note != '') {
+      this.listNotes.push(this.note);
     }
   }
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  /* Método que se llama al activar o desactivar el checkbox */
+  onchange(id: number, index: number) {
+    let noteSelected = this.listNotes[index];
+    noteSelected.id == id
+      ? (noteSelected.complete = !noteSelected.complete)
+      : noteSelected.complete;
+
+    //Ternario para saber si la tarea está completada, si lo está, se envía de primero,
+    //sino se envía al final de la lísta.
+    noteSelected.complete
+      ? this.listNotes.unshift(this.listNotes.splice(index, 1)[0])
+      : this.listNotes.push(this.listNotes.splice(index, 1)[0]);
+    console.log('-------------- Acción del checkbox ----------------');
+    console.log(this.listNotes);
   }
 
-  onchange(id:number, index: number){
-    this.listNotes.forEach((note:Note)=>{
-      note.id == id? note.complete = !note.complete: note.complete
-    });
+  delete(index: number) {
+    // Se hace uso de la librería Sweetalert para mostrar un mensaje de confirmación
+    swal
+      .fire({
+        title: 'Eliminar nota',
+        text: '¿Estás seguro que deseas eliminar esta nota?',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Si, quiero eliminar esta nota',
+      })
+      .then((result) => {
+        if (result.isConfirmed) {
+          this.isConfirmed(index);
+        }
+      });
+  }
 
+  isConfirmed(index: number) {
+    /* Quitamos el elemento de la lista */
+    this.listNotes.splice(index, 1);
+    console.log('-------------- Eliminar un elemento ----------------');
+    console.log(this.listNotes);
+
+    swal.fire('¡Nota eliminada!', '', 'success');
+    swal.fire('Eliminada', 'Esta nota ha sido eliminada.', 'success');
   }
 }
